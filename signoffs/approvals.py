a4f5fb226f7a2ac78d569e2acc26850099f94b63
powderflask <powderflask@gmail.com>
2022-09-29 22:59:33 -0700

    refactor approval type API to make business logic composable.

--- signoffs/approvals.py
+++ signoffs/approvals.py
@@ -4,7 +4,7 @@
 from django.apps import apps
 
 from signoffs.core.approvals import (
-    BaseApproval,
+    BaseApproval, DefaultApprovalBusinessLogic, ApprovalLogic,
     user_can_revoke_approval,
 )
 from signoffs.core.renderers import (
