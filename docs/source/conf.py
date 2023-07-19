d960fdfea1c8fbd448e89e946cefb9efed8925db
Ben Blair <benchrisblair@users.noreply.github.com>
2023-07-19 09:40:11 -0700

    Add docs directory

---
+++ docs/source/conf.py
@@ -0,0 +1,42 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Django Signoffs"
+copyright = "2022, Joseph Fall"
+author = "Joseph Fall"
+
+# The short X.Y version.
+version = "0.2.0"
+# The full version, including alpha/beta/rc tags.
+release = "0.2.0"
+
+# The master toctree document.
+master_doc = "index"
+
+# The suffix(es) of source filenames.
+source_suffix = [".rst", ".md"]
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "myst_parser",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
---
+++ docs/source/index.md
@@ -0,0 +1,8 @@
+# {{cookiecutter.project_name}} Homepage
+
+```{toctree}
+:maxdepth: 2
+:caption: "Contents:"
+
+# insert pages here
+```
