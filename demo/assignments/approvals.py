2ce7cf84fa3d3c68153dd165d62a1338b33097a3
JacobTumak <jacobtumak@gmail.com>
2024-05-14 18:39:48 -0700

    Fixed bugs with assignments, added status info to detail and list view

--- demo/assignments/approvals.py
+++ demo/assignments/approvals.py
@@ -3,6 +3,9 @@ from signoffs.models import ApprovalSignet
 from signoffs.registry import register
 from signoffs.signing_order import SigningOrder
 
+from django.contrib.auth.models import User
+from django.utils.functional import SimpleLazyObject
+
 
 @register("assignments.approvals.NewAssignmentApproval")
 class NewAssignmentApproval(SimpleApproval):
@@ -41,3 +44,16 @@ class NewAssignmentApproval(SimpleApproval):
         submit_completed_signoff,  # submitted
         confirm_completion_signoff,  # completed - unrevokable?
     )
+
+    def next_signoffs(self, for_user=None):
+        if not for_user:
+            return super().next_signoffs()
+        if not type(for_user) in (User, SimpleLazyObject):
+            raise TypeError(f"var \"for_user\" must be User instance, instead got {type(for_user)}\n")
+
+        assignment = self.subject
+        if (for_user == assignment.assigned_by and assignment.status in ['draft', 'pending_review']) or (for_user == assignment.assigned_to and assignment.status in ['requested', 'in_progress']):
+            return super().next_signoffs(for_user=for_user)
+        else:
+            return []
+
--- demo/assignments/forms.py
+++ demo/assignments/forms.py
@@ -7,9 +7,9 @@ class AssignmentForm(ModelForm):
     class Meta:
         model = Assignment
         fields = ["assignment_name", "assigned_to", "details"]
-        hidden = ["assigned_by"]
+        # hidden = ["assigned_by"]
         widgets = {
             "assignment_name": TextInput(attrs={"style": "width:75%"}),
             "details": Textarea(attrs={"rows": 4, "style": "width:100%"}),
-            "assigned_by": HiddenInput(),
+            # "assigned_by": HiddenInput(),
         }
