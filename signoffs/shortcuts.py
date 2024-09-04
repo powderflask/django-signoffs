5e4c0aeb452e9e8be0254015a3436e5f270d0a90
JacobTumak <jacobtumak@gmail.com>
2024-09-04 16:51:13 -0700

    Added **kwargs and removed signet_pk and stamp_pk as pos. args

--- signoffs/shortcuts.py
+++ signoffs/shortcuts.py
@@ -16,9 +16,11 @@ if TYPE_CHECKING:
     from signoffs.core.approvals import AbstractApproval
 
 
-def get_signet_or_404(signoff_type, signet_pk):
-    """Return Signet with given pk, for the given Signoff Type or id, or raise Http404"""
-    kwargs.update(pk=signet_pk) if signet_pk else None
+def get_signet_or_404(signoff_type, **kwargs):
+    """
+    Return Signet with given pk, for the given Signoff Type or id, or raise Http404
+    """
+    # kwargs.update(pk=signet_pk) if signet_pk else None
     signoff = registry.get_signoff_type(signoff_type)
     if signoff is None:
         raise Http404(f"No registered signoff with id: {signoff_type}")
@@ -28,16 +30,23 @@ def get_signet_or_404(signoff_type, signet_pk):
     )
 
 
-def get_signoff_or_404(signoff_type: str | AbstractSignoff, signet_pk=None, **kwargs) -> AbstractSignoff:
-    """Return Signoff of given type or id, backed by Signet with the given pk, or raise Http404"""
-    kwargs.update(pk=signet_pk) if signet_pk else None
+def get_signoff_or_404(signoff_type: str | AbstractSignoff, **kwargs) -> AbstractSignoff:
+    """
+    kwargs passed to get_signet_or_404.
+
+    Return Signoff of given type or id, backed by Signet with the given pk, or raise Http404
+    """
+    # signet_pk = kwargs.pop("signet_pk", None)
+    # kwargs.update(pk=signet_pk) if signet_pk else None
     signet = get_signet_or_404(signoff_type, **kwargs)
     return signet.signoff
 
 
-def get_approval_stamp_or_404(approval_type: str | AbstractApproval, stamp_pk=None, **kwargs) -> AbstractApprovalStamp:
-    """Return ApprovalStamp instance with given pk for the given Approval Type or id, or raise Http404"""
-    kwargs.update(pk=stamp_pk) if stamp_pk else None
+def get_approval_stamp_or_404(approval_type: str | AbstractApproval, **kwargs) -> AbstractApprovalStamp:
+    """
+    Return ApprovalStamp instance with given pk for the given Approval Type or id, or raise Http404
+    """
+    # kwargs.update(pk=stamp_pk) if stamp_pk else None
     approval = registry.get_approval_type(approval_type)
     if approval is None:
         raise Http404(f"No registered approval with id: {approval_type}")
@@ -47,8 +56,12 @@ def get_approval_stamp_or_404(approval_type: str | AbstractApproval, stamp_pk=No
     )
 
 
-def get_approval_or_404(approval_type: str | AbstractApproval, stamp_pk=None, **kwargs) -> AbstractApproval:
-    """Return Approval of given type or id, backed by StampModel with given pk, or raise Http404"""
-    kwargs.update(pk=stamp_pk) if stamp_pk else None
+def get_approval_or_404(approval_type: str | AbstractApproval, **kwargs) -> AbstractApproval:
+    """
+    kwargs passed to get_approval_stamp_or_404.
+
+    Return Approval of given type or id, backed by StampModel with given pk, or raise Http404
+    """
+    # kwargs.update(pk=stamp_pk) if stamp_pk else None
     stamp = get_approval_stamp_or_404(approval_type, **kwargs)
     return stamp.approval
